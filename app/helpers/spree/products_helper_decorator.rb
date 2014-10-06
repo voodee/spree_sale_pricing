e2f51c2d10e50bc784420c16b45e3c60184ab47c
Eric Saupe <ericsaupe@gmail.com>
2014-10-06 09:39:28 -0600

    Added a check for less than 0 msrp

--- app/helpers/spree/products_helper_decorator.rb
+++ app/helpers/spree/products_helper_decorator.rb
@@ -1,5 +1,5 @@
 def discount_percentage(product)
-  if product.master.msrp.present?
+  if product.master.msrp.present? && (product.master.msrp.to_f > 0)
     "#{((1 - product.price.to_f / product.master.msrp.to_f) * 100).round}%"
   end
 end
