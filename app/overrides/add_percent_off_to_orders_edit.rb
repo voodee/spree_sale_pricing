c30d618fd53d192e304dd642bc1198d0cb312cf6
Thomas I. Sample <thomas@mfrwdesign.com>
2014-10-30 03:07:21 -0400

    Fix show percentage savings on cart page

--- app/overrides/add_percent_off_to_orders_edit.rb
+++ app/overrides/add_percent_off_to_orders_edit.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(virtual_path: "spree/orders/edit",
+Deface::Override.new(virtual_path: "spree/orders/_line_item",
                      name: "add_percent_off_to_orders_edit",
                      insert_bottom: "[data-hook='cart_item_price']",
                      partial: "spree/shared/percent_off_variant",
