201d4775134625b36e25d146ec630981471e2f62
Tim <thogg4@gmail.com>
2015-02-23 16:22:25 -0700

    get data from elasticsearch

--- app/helpers/spree/products_helper_decorator.rb
+++ app/helpers/spree/products_helper_decorator.rb
@@ -8,7 +8,7 @@ end
 
 def discount_percentage_variant(variant)
   if variant.msrp.present? && (variant.msrp.to_f > 0)
-    "#{((1 - variant.price.to_f / variant.msrp.to_f) * 100).round}%"
+    "#{((1 - variant.price.amount.to_f / variant.msrp.to_f) * 100).round}%"
   else
     return '0%'
   end
