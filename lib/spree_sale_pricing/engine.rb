7b99ef4cea43a3f95cfcaac4b25a41d0d247f2ea
Eric Saupe <ericsaupe@gmail.com>
2015-05-07 10:45:44 -0600

    Added PercentOnLineItemMsrp Calculator

--- lib/spree_sale_pricing/engine.rb
+++ lib/spree_sale_pricing/engine.rb
@@ -9,6 +9,10 @@ module SpreeSalePricing
       g.test_framework :rspec
     end
 
+    initializer "spree.register.msrp_calculators", :after => 'spree.register.calculators' do |app|
+      app.config.spree.calculators.shipping_methods << Spree::Calculator::PercentOnLineItemMsrp
+    end
+
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
