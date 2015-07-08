1fbc6964a1529c5bba8b20c7dbd25c2e37c8aee3
Mike Bourgeous <mike-bourgeous@users.noreply.github.com>
2015-07-08 15:17:08 -0600

    Don't show red price if MSRP is greater than or equal to price.

--- app/helpers/spree/products_helper_decorator.rb
+++ app/helpers/spree/products_helper_decorator.rb
@@ -1,5 +1,5 @@
 def discount_percentage(product)
-  if product.master.msrp.present? && (product.master.msrp.to_f > 0)
+  if product.master.msrp.present? && (product.master.msrp.to_f > product.price.to_f)
     "#{((1 - product.price.to_f / product.master.msrp.to_f) * 100).round}"
   else
     return '0'
@@ -7,10 +7,11 @@ def discount_percentage(product)
 end
 
 def discount_percentage_variant(variant)
-  if variant.msrp.present? && (variant.msrp.to_f > 0)
-    # this check is because of a oddity in our elasticsearch gem. we should fix it
-    # We also check if it is an instance of Float becuase spree-product-assemblies is somehow returning a Float
-    "#{((1 - ((variant.price.instance_of?(BigDecimal) || variant.price.instance_of?(Float)) ? variant.price.to_f : variant.price.amount.to_f) / variant.msrp.to_f) * 100).round}"
+  price = variant.price.respond_to?(:to_f) ? variant.price.to_f : variant.price.amount.to_f
+  msrp = variant.msrp.try(:to_f)
+
+  if msrp && variant.price && (msrp > price)
+    "#{((1 - price / msrp) * 100).round}"
   else
     return '0'
   end
