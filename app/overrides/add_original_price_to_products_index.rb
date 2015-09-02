5274f4930e1ae9995b058a0b4be954308ada8a76
Tim <thogg4@gmail.com>
2015-09-02 12:06:31 -0600

    move display_variant into nibley

--- app/overrides/add_original_price_to_products_index.rb
+++ app/overrides/add_original_price_to_products_index.rb
@@ -3,9 +3,3 @@ Deface::Override.new(virtual_path: "spree/shared/_products",
                      replace_contents: "p.listing-price",
                      partial: "spree/shared/original_price_display",
                      disabled: false)
-
-Deface::Override.new(virtual_path: "spree/shared/_listing_variant",
-                     name: "add_original_price_to_products_index_variants",
-                     replace_contents: ".listing-variant-price",
-                     partial: "spree/shared/original_price_display_variant",
-                     disabled: false)
